0fe04c027aec36cfc40f04a52653acbb96c38a7f
Trent Stevens <trent.stevens1@gmail.com>
2017-07-27 18:43:54 +1200

    Initial Commit

---
+++ Tool Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tool Collector
+//
+//  Created by Trent Stevens on 27/07/17.
+//  Copyright © 2017 Trent Stevens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
